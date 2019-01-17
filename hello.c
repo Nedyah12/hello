f05839b135544720be29246be70aea19b358a4f2
Nedyah12 <42650110+Nedyah12@users.noreply.github.com>
2019-01-16 22:02:52 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Hayden Henning, Azan
+ * Date: 1/15/2018
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello, Hayden Henning and Azan\n");
+
+  return 0;
+}
